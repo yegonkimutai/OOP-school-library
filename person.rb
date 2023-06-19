7fe1c1ffa0b420490a9f2de1f905e516b2c294ae
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-19 11:46:00 +0300

    :sparkles:Add persons class

---
+++ person.rb
@@ -0,0 +1,22 @@
+class Persons
+    def initialize(id, age, parent_permission = true, name = "Unknown")
+        @id = id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    attr_reader :id
+
+    attr_accessor :name  
+
+    attr_accessor :age 
+
+    def of_age?
+        @age >= 18
+    end
+
+    def can_use_services?
+        of_age? || @parent_permission
+    end
+end
