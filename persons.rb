a848513e799bffe36ea9c55be8d613b252a4b159
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-19 12:33:14 +0300

    :wrench:Fix linters

--- persons.rb
+++ persons.rb
@@ -1,34 +1,23 @@
 class Persons
-    def initialize(id, age, parent_permission = true, name = "Unknown")
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
+    def initialize(id, age, parent_permission: true, name = 'Unknown')
+      @id = id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
     end
-
+  
     attr_reader :id
-
-    attr_accessor :name  
-
-    attr_accessor :age 
-
+  
+    attr_accessor :name
+  
+    attr_accessor :age
+  
     def of_age?
-        @age >= 18
+      @age >= 18
     end
-
+  
     def can_use_services?
-        of_age? || @parent_permission
+      of_age? || @parent_permission
     end
-end
-
-
-class Teacher < Persons
-    def initialize(id, age, specialization, parent_permission = true, name = "Unknown")
-        super(id, age, parent_permission, name)
-        @specialization = specialization
-    end
-
-    def can_use_services?
-        true
-    end
-end
+  end
+  
