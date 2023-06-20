867565282a23cf300af64cde2c3bf5906c11fa9c
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-20 13:11:57 +0300

    :wrench:Fix linters

--- persons.rb
+++ persons.rb
@@ -6,6 +6,7 @@ class Persons < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_reader :id
@@ -26,38 +27,3 @@ class Persons < Nameable
     @age >= 18
   end
 end
-
-class Nameable
-  def correct_name
-    raise NotImplementedError
-  end
-end
-
-class Decorator < Nameable
-  attr_accessor :nameable
-
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    words = @nameable.correct_name
-    words.length > 10 ? words.chars[0..9].join : words
-  end
-end
-
-person = Persons.new(250, 15, 'false', 'maximilianus')
-
