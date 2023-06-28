acea2970462790f22de46d88cf6d137e5ebb17fe
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-28 10:51:08 +0300

    Add test for book method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,5 +11,10 @@ describe Book do
             new_book = Book.new('Title', 'Author')
             expect(new_book.author).to eq('Author')
         end
+
+        it 'Should return empty rental' do
+            new_book = Book.new('Title', 'Author')
+            expect(new_book.rentals).to be_empty
+        end
     end
 end
